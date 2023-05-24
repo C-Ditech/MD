8aec1f62f222527383cad8fda8736bd2aacc9a9b
baguskf <20102054@ittelkom-pwt.ac.id>
2023-05-24 23:41:05 +0700

    revisi tampilan buttom nav

--- app/src/main/java/com/example/mycapstone/CameraActivity.kt
+++ app/src/main/java/com/example/mycapstone/CameraActivity.kt
@@ -1,11 +1,26 @@
 package com.example.mycapstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mycapstone.databinding.ActivityCameraBinding
+import com.example.mycapstone.databinding.ActivityMainBinding
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        binding.button2.setOnClickListener {
+            val intent = Intent(this@CameraActivity, intencobaAcitvity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
